de11cc830b27ddb292887bc4640789286b1fbdab
f0x <f@0x52.eu>
2018-05-24 17:04:37 +0200

    shame on me for not commiting earlier

--- src/js/app.js
+++ src/js/app.js
@@ -3,15 +3,15 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const rfetch = require('fetch-retry');
+const Promise = require('bluebird');
 
 require('../scss/layout.scss');
 
-let create = require('create-react-class');
+const create = require('create-react-class');
 
-let urllib = require('url');
-let apiUrl = urllib.parse("https://neo.lain.haus/api/report");
-let neo = require('../assets/neo_full_bg.png');
-const icons = require('./icons.js');
+const urllib = require('url');
+const blank = require('../assets/blank.jpg');
+//const icons = require('./icons.js');
 
 let options = {retries: 5, retryDelay: 200};
 
@@ -19,339 +19,337 @@ let App = create({
   displayName: "App",
 
   getInitialState: function() {
-    return({
-      json: undefined
-    });
-  },
-
-  setRef: function(e) {
-    e.addEventListener('keydown', this.onKey);
-    e.focus();
-    this.setState({
-      ref: e
-    });
-  },
-
-  componentDidMount: function() {
-    setTimeout(() => {
-      if (window.location.hash) {
-        var hash = window.location.hash.substring(1);
-        this.state.ref.value = hash;
-        this.submit();
-      }
-    }, 100);
-  },
-
-  onKey: function(e) {
-    if (e.keyCode == 13) {
-      this.submit();
+    let data = [null, null];
+    if (localStorage.getItem("data") != undefined) {
+      data = JSON.parse(localStorage.getItem("data"));
     }
+    return({
+      json: undefined,
+      data: data
+    });
   },
 
-  submit: function() {
-    let url = Object.assign(apiUrl, {
+  getUserInfo: function(id, data) {
+    let info = data[id];
+    let url = urllib.format(Object.assign({}, info.hs, {
+      pathname: `/_matrix/client/r0/profile/${info.login.user_id}/displayname`,
       query: {
-        server_name: this.state.ref.value
+        access_token: info.login.access_token
       }
-    });
-    
-    this.setState({
-      loading: true,
-      json: undefined
-    });
+    }));
 
-    rfetch(urllib.format(url), options)
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json);
-        this.setState({
-          json: json,
-          loading: false
+    this.nameFetch = rfetch(url, options)
+      .then(response => response.json())
+      .then(responseJson => {
+        info.display_name = info.login.user_id;
+        if (responseJson.displayname != undefined) {
+          info.display_name = responseJson.displayname;
+        }
+        let data = this.state.data;
+        data[id] = info;
+        this.setState({data: data});
+      });
+
+    url = urllib.format(Object.assign({}, info.hs, {
+      pathname: `/_matrix/client/r0/profile/${info.login.user_id}/avatar_url`,
+      query: {
+        access_token: info.login.access_token
+      }
+    }));
+
+    this.imgFetch = rfetch(url, options)
+      .then(response => response.json())
+      .then(responseJson => {
+        info.img = blank;
+        if(responseJson.errcode == undefined &&
+          responseJson.avatar_url != undefined) {
+          info.img = m_thumbnail(info.hs, responseJson.avatar_url, 256, 256);
+        }
+        let data = this.state.data;
+        data[id] = info;
+        this.setState({data: data});
+        localStorage.setItem("data", JSON.stringify(data));
+      });
+  },
+
+  getRoomlist: function(id, data) {
+    let info = data[id];
+    let localRooms = {};
+    let url = urllib.format(Object.assign({}, info.hs, {
+      pathname: "/_matrix/client/r0/joined_rooms",
+      query: {
+        access_token: info.login.access_token
+      }
+    }));
+
+    rfetch(url, options)
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error('Error:', error);
+      })
+      .then((responseJson) => {
+        Promise.map(responseJson.joined_rooms, (roomId) => {
+          return this.getRoomInfo(info, roomId);
+        }).then((roomInfoArray) => {
+          roomInfoArray.forEach((roomInfo) => {
+            localRooms[roomInfo[0]] = {
+              name: roomInfo[1],
+              img: roomInfo[2]
+            };
+          });
+
+          let data = this.state.data;
+          info.rooms = localRooms;
+          data[id] = info;
+          this.setState({data: data});
+          localStorage.setItem("data", JSON.stringify(data));
         });
       });
   },
 
-  render: function() {
-    let result;
-    let errors;
-    let active;
+  getRoomInfo: function(info, roomId) {
+    return Promise.all([
+      roomId,
+      this.getName(info, roomId),
+      this.getPic(info, roomId)
+    ]);
+  },
 
-    if (this.state.json != undefined) {
-      if (this.state.json != undefined) {
-        result = <TestResults json={this.state.json}/>;
-      }
+  getName: function(info, roomId) {
+    return new Promise((resolve, reject) => {
+      let url = urllib.format(Object.assign({}, info.hs, {
+        pathname: `/_matrix/client/r0/rooms/${roomId}/state/m.room.name`,
+        query: {
+          access_token: info.login.access_token
+        }
+      }));
 
-      if (Object.keys(this.state.json.ConnectionReports) == 0 &&
-        Object.keys(this.state.json.ConnectionReports) == 0) {
-        errors = (
-          <div className="error">
-            Probably not a matrix server
-          </div>
-        );
-      }
-    }
-
-    if (this.state.loading) {
-      active = "active";
-    }
-
-    return (
-      <div className="block">
-        <a href="https://neo.lain.haus" target="_blank"><img src={neo}/></a>
-        <div className="text">
-          <span id="jok">Unlike the name suggests, this won't help you find three letter agencies :P</span><br/>
-          <a href="https://github.com/f0x52/fed-tester">Sourcecode on Github</a>
-        </div>
-        <div className="input">
-          <input ref={this.setRef}/>
-          <div className={"sk-cube-grid " + active} onClick={this.submit}>
-            <span>Go</span>
-            <div className="sk-cube sk-cube1"></div>
-            <div className="sk-cube sk-cube2"></div>
-            <div className="sk-cube sk-cube3"></div>
-            <div className="sk-cube sk-cube4"></div>
-            <div className="sk-cube sk-cube5"></div>
-            <div className="sk-cube sk-cube6"></div>
-            <div className="sk-cube sk-cube7"></div>
-            <div className="sk-cube sk-cube8"></div>
-            <div className="sk-cube sk-cube9"></div>
-          </div>
-        </div>
-        {result}
-        {errors}
-      </div>
-    );
-  }
-});
-
-let TestResults = create({
-  displayName: "TestResults",
-
-  render: function() {
-    return (
-      <div className="results">
-        <ConnectionErrors json={this.props.json.ConnectionErrors}/>
-        <ConnectionReports json={this.props.json.ConnectionReports}/>
-        <DNSResult json={this.props.json.DNSResult}/>
-      </div>
-    );
-  }
-});
-
-let ConnectionReports = create({
-  displayName: "ConnectionErrors",
-
-  render: function() {
-    let j = this.props.json;
-    let connections = Object.keys(j).map((ip, id) => {
-      let info = j[ip];
-      return <ReportTable info={info} key={id} ip={ip}/>;
-    });
-    return (
-      <div className="connection">
-        <h2>Connection Reports</h2>
-        {connections}
-      </div>
-    );
-  }
-});
-
-let ConnectionErrors = create({
-  displayName: "ConnectionErrors",
-
-  render: function() {
-    let j = this.props.json;
-    if (Object.keys(j).length == 0) {
-      return null;
-    }
-    let connections = Object.keys(j).map((ip, id) => {
-      let info = j[ip];
-      return <ReportTable info={info} key={id} ip={ip}/>;
-    });
-    return (
-      <div className="connection err">
-        <h2>Connection Errors</h2>
-        {connections}
-      </div>
-    );
-  }
-});
-
-let ReportTable = create({
-  displayName: "ReportTable",
-
-  getInitialState: function() {
-    return ({
-      collapsed: this.props.info.Checks.AllChecksOK
+      fetch(url)
+        .then(response => response.json())
+        .catch((error) => {
+          console.error(error);
+          reject(error);
+        })
+        .then(responseJson => {
+          if (responseJson.name != undefined) {
+            resolve(responseJson.name);
+          } else {
+            resolve(roomId);
+          }
+        });
     });
   },
 
-  toggle: function() {
-    let collapsed = this.state.collapsed;
-    if (collapsed) {
-      collapsed = false;
-    } else {
-      collapsed = true;
-    }
-    this.setState({
-      collapsed: collapsed
+  getPic: function(info, roomId) {
+    return new Promise((resolve, reject) => {
+      let url = urllib.format(Object.assign({}, info.hs, {
+        pathname: `/_matrix/client/r0/rooms/${roomId}/state/m.room.avatar`,
+        query: {
+          access_token: info.login.access_token
+        }
+      }));
+
+      fetch(url)
+        .then(response => response.json())
+        .catch((error) => {
+          console.error(error);
+          reject(error);
+        })
+        .then(responseJson => {
+          if (responseJson.errcode == undefined) {
+            resolve(m_download(info.hs, responseJson.url));
+          } else {
+            resolve(blank);
+          }
+        });
     });
   },
 
+  login: function(id, data) {
+    let hs = urllib.parse(data.hs);
+    let url = urllib.format(Object.assign(hs, {
+      pathname: "/_matrix/client/r0/login"
+    }));
+
+    let body = {
+      "user": data.user,
+      "password": data.pass,
+      "type": "m.login.password",
+      "initial_device_display_name": "Matrix Migration",
+    };
+
+    rfetch(url, {
+      body: JSON.stringify(body),
+      headers: {
+        'content-type': 'application/json'
+      },
+      method: 'POST',
+    }, options).then((response) => response.json())
+      .then((responseJson) => {
+        let data = this.state.data;
+        data[id] = {};
+        data[id].login = responseJson;
+        data[id].hs = hs;
+        this.setState({
+          data: data
+        });
+        this.getUserInfo(id, data);
+        this.getRoomlist(id, data);
+      })
+      .catch((error) => {
+        this.setState({json: {error: error}});
+        console.error(error);
+      });
+  },
+
   render: function() {
-    let checks = <TableFromObject object={this.props.info.Checks} collapsed={this.state.collapsed} tree={1}/>;
-    let symbol = "Error";
-    let className = "false";
-    let icon = icons.right;
+    return (
+      <div className="container">
+        <div className="split">
+          <From id={0} login={this.login} data={this.state.data[0]}/>
+        </div>
 
-    if (!this.state.collapsed) {
-      icon = icons.down;
+        <div className="split">
+          <To id={1} login={this.login} data={this.state.data[1]}/>
+        </div>
+        <div className="migrate">
+          Migrate
+        </div>
+      </div> 
+    );
+  }
+});
+
+let From = create({
+  displayName: "From",
+
+  render: function() {
+    if (this.props.data != null) {
+      let rooms = <span>Loading rooms</span>;
+      if (this.props.data.rooms != undefined) {
+        rooms = Object.keys(this.props.data.rooms).map((roomId) => {
+          let room = this.props.data.rooms[roomId];
+          return (
+            <div className="row" key={roomId}>
+              <img src={room.img}/>
+              <span>{room.name}</span>
+              <div className="checkbox">
+                <input id={roomId} type="checkbox"/><label htmlFor={roomId}/>
+              </div>
+            </div>
+          );
+        });
+      }
+  
+      return (
+        <div className="profile">
+          <div className="line">
+            <h2>{this.props.data.display_name}</h2>
+            <div className="checkbox">
+              <input id="c1" type="checkbox"/><label htmlFor="c1"/>
+            </div>
+          </div>
+          <div className="line">
+            <img src={this.props.data.img}/>
+            <div className="checkbox">
+              <input id="c2" type="checkbox"/><label htmlFor="c2"/>
+            </div>
+          </div>
+          <div className="table">
+            {rooms}
+          </div>
+        </div>
+      );
     }
+    return (
+      <LoginForm id={this.props.id} login={this.props.login}/>
+    );
+  }
+});
 
-    if (this.props.info.Checks.AllChecksOK) {
-      symbol = "Success";
-      className = "true";
-    }
+let To = create({
+  displayName: "To",
 
+  render: function() {
     return (
       <div>
-        <h3>{this.props.ip}</h3>
-        <div className="table">
-          <div className="head">
-            {icon}
-            Check
-          </div>
-          <div className="body">
-            <div className="row toggle" onClick={this.toggle}>
-              {icon}
-              <div className="col">All Checks OK</div>
-              <div className={"col bool " + className}>{symbol}</div>
-            </div>
-            {checks}
-          </div>
-        </div>
+        <h2>{this.props.data.display_name}</h2>
+        <img src={this.props.data.img}/>
       </div>
     );
   }
 });
 
-let TableFromObject = create({
-  displayName: "TableFromObject",
+let LoginForm = create({
+  displayName: "LoginForm",
 
   getInitialState: function() {
     return ({
-      collapsed: this.props.collapsed
+      active: "",
+      user: "",
+      pass: "",
+      hs: "https://matrix.org",
     });
   },
 
-  toggle: function() {
-    let collapsed = this.state.collapsed;
-    if (collapsed) {
-      collapsed = false;
-    } else {
-      collapsed = true;
-    }
+  handleUser: function(event) {
+    this.setState({user: event.target.value});
+  },
+
+  handlePass: function(event) {
+    this.setState({pass: event.target.value});
+  },
+
+  handleHs: function(event) {
+    this.setState({hs: event.target.value});
+  },
+
+  submit: function() {
     this.setState({
-      collapsed: collapsed
+      active: "active"
     });
+    this.props.login(this.props.id, this.state);
   },
 
   render: function() {
-    let objectArray = Object.keys(this.props.object);
-    return objectArray.map((check, id) => {
-      let symbol = "Error";
-      let className = "false";
-      if (this.props.object[check]) {
-        symbol = "Success";
-        className = "true";
-      }
-    
-      if (check == "AllChecksOK") {
-        return null;
-      } else if (!this.props.collapsed) {
-        if (typeof(this.props.object[check]) == "boolean") {
-          return (
-            <div className={"row toggle tree-" + this.props.tree} key={id}>
-              <div className="col">{check}</div>
-              <div className={"col bool " + className}>{symbol}</div>
-            </div>
-          );
-        } else {
-          return (
-            <div key={id}>
-              <div className={"row toggle tree-" + this.props.tree} onClick={this.toggle}>
-                <div className="col">{check}</div>
-              </div>
-              <TableFromObject object={this.props.object[check]} collapsed={false} key={id} tree={this.props.tree+1} />
-            </div>
-          );
-        }
-      }
-      return null;
-    });
-  }
-});
-
-let DNSResult = create({
-  displayName: "DNS",
-
-  render: function() {
-    let j = this.props.json;
-    if (j.SRVRecords == null) {
-      return (
-        <div className="dns">
-          <h2>No SRV Records</h2>
-        </div>
-      );
-    }
-
-    let records = j.SRVRecords.map((record, id) => {
-      return (
-        <div className="row" key={id}>
-          <div className="col">{record.Target}</div>
-          <div className="col">{record.Port}</div>
-          <div className="col">{record.Priority}</div>
-          <div className="col">{record.Target}</div>
-        </div>
-      );
-    });
-
-    let hosts = Object.keys(j.Hosts).map((host) => {
-      return j.Hosts[host].Addrs.map((address, id) => {
-        return (
-          <div className="row" key={id}>
-            <div className="col">{address}</div>
-          </div>
-        );
-      });
-    });
-
+    let active = this.state.active;
     return (
-      <div className="dns">
-        <h2>{j.SRVCName} records:</h2>
-        <div className="table">
-          <div className="header">
-            <span className="col">Target</span>
-            <span className="col">Port</span>
-            <span className="col">Priority</span>
-            <span className="col">Target</span>
-          </div>
-          <div className="body">
-            {records}
-          </div>
-        </div>
-        <div className="table">
-          <div className="head">
-            Address
-          </div>
-          <div className="body">
-            {hosts}
-          </div>
+      <div className="input">
+        <input className="noSubmit" value={this.state.user} onChange={this.handleUser} placeholder="Username"/><br/>
+        <input className="noSubmit" value={this.state.pass} onChange={this.handlePass} placeholder="Password" type="password"/><br/>
+        <input value={this.state.hs} onChange={this.handleHs} placeholder="Homeserver"/>
+        <div className={"sk-cube-grid " + active} onClick={this.submit}>
+          <span>Go</span>
+          <div className="sk-cube sk-cube1"></div>
+          <div className="sk-cube sk-cube2"></div>
+          <div className="sk-cube sk-cube3"></div>
+          <div className="sk-cube sk-cube4"></div>
+          <div className="sk-cube sk-cube5"></div>
+          <div className="sk-cube sk-cube6"></div>
+          <div className="sk-cube sk-cube7"></div>
+          <div className="sk-cube sk-cube8"></div>
+          <div className="sk-cube sk-cube9"></div>
         </div>
       </div>
     );
   }
 });
+
+function m_thumbnail(hs, mxc, w, h) {
+  return urllib.format(Object.assign({}, hs, {
+    pathname: `/_matrix/media/r0/thumbnail/${mxc.substring(6)}`,
+    query: {
+      width: w,
+      height: h
+    }
+  }));
+}
+
+function m_download(hs, mxc) {
+  return urllib.format(Object.assign({}, hs, {
+    pathname: `/_matrix/media/r0/download/${mxc.substring(6)}`
+  }));
+}
 
 ReactDOM.render(
   <App />,
